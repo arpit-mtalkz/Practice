f32e414a116a3b171c50a6d853c9f249a925bdc4
arpit-mtalkz <arpitrajput57@gmail.com>
2023-01-31 18:00:11 +0530

    Eleventh commit

---
+++ Flask/tut.py
@@ -0,0 +1,19 @@
+import Contact_book 
+
+object1=Contact_book.phonebook()
+
+object2=Contact_book.menu()
+
+object3=Contact_book.add_contact()
+
+object4=Contact_book.delete_contact()
+
+object5=Contact_book.delete_all()
+
+object6=Contact_book.search_contact()
+
+object7=Contact_book.update_contact()
+
+
+
+
